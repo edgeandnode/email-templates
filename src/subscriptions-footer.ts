dec5de0c4be5718cd649a94b14f39305e0b3c228
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-07-10 23:28:16 +0200

    Remove hr

--- src/subscriptions-footer.ts
+++ src/subscriptions-footer.ts
@@ -2,7 +2,7 @@ import { footer } from './footer'
 
 const markdown = `
 
----
+<br />
 
 If you have any questions or need help, please review the [The Graph’s Docs](https://thegraph.com/docs/en/), \
 or contact us at [info@edgeandnode.com](mailto:info@edgeandnode.com).
