4ce197e7fbca5f84131f12a382943ca7ef3288c3
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-07-10 22:47:45 +0200

    Bump CTA text opacity

--- src/email-cta.ts
+++ src/email-cta.ts
@@ -1,4 +1,4 @@
 export const emailCta = [
   /\bemail-cta\b/g,
-  'style="padding: 16px 24px; display: inline-block; background: #6148E0; color: rgba(255, 255, 255, 0.88); border-radius: 4px; text-decoration: none; font-weight: 600;"',
+  'style="padding: 16px 24px; display: inline-block; background: #6148E0; color: #fff; border-radius: 4px; text-decoration: none; font-weight: 600;"',
 ] as const
