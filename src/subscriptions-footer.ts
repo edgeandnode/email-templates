9b663b3f946138fd00db7d8fab84e50ebe247b65
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-07-10 23:25:10 +0200

    Tweak whitespace

--- src/subscriptions-footer.ts
+++ src/subscriptions-footer.ts
@@ -2,8 +2,12 @@ import { footer } from './footer'
 
 const markdown = `
 
+---
+
 If you have any questions or need help, please review the [The Graph’s Docs](https://thegraph.com/docs/en/), \
 or contact us at [info@edgeandnode.com](mailto:info@edgeandnode.com).
+
+<br />
 
 ${footer}
 
