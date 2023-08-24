a02b8712874e61fe304d197395a4d81a574d0c56
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-08-24 18:17:10 +0200

    Add a hr between footer message and email content

--- src/footer.ts
+++ src/footer.ts
@@ -1,8 +1,11 @@
 export const footer = `
 
 Thanks,
+
 Edge & Node
 
-If you’d like to opt out of future emails, visit the [Settings page](https://thegraph.com/studio/settings/) in Subgraph Studio to unsubscribe from notifications.
+---
+
+<small>If you’d like to opt out of future emails, visit the [Settings page](https://thegraph.com/studio/settings/) in Subgraph Studio to unsubscribe from notifications.</small>
 
 `.trim()
