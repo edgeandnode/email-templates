651cb5f656b686527070ddff845d85380ea1ed29
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-03-06 14:38:08 +0100

    Add an email template for unpaid invoice

---
+++ emails/unpaid-invoice.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+import { MarkdownEmail } from '../src/MarkdownEmail'
+
+export default function SubscriptionRenewalProcessed() {
+  return <MarkdownEmail filename={__filename} />
+}
